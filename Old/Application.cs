6195c480a4f51cec7b5185f8be2916cac324693b
Uwe Riegel <uriegel@hotmail.de>
2022-04-26 21:53:46 +0200

    Cleaned up

--- Old/Application.cs
+++ Old/Application.cs
@@ -10,19 +10,6 @@ namespace GtkDotNet
 {
     public class Application
     {
-        public Application(string id)
-            => app = Raw.Application.New(id);       
-
-        public int Run(Action onActivate)
-            => Raw.Application.Run(app, onActivate);
-
-        public void AddActions(IEnumerable<GtkAction> actions)
-            => Raw.Application.AddActions(app, actions);
-
-        public void Quit() => Raw.Application.Quit(app);
-
-        public void AddWindow(Window window) => Raw.Application.AddWindow(app, window.handle);
-
         public bool RegisterResources()
         {
             var assembly = Assembly.GetEntryAssembly();
