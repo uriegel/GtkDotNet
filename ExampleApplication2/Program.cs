9aabe04560a243ae7008764465dc6079295ca62c
Uwe Riegel <uriegel@hotmail.de>
2022-04-28 21:27:55 +0200

    Example 2

---
+++ ExampleApplication2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using GtkDotNet;
+
+var app = Application.New("org.gtk.example");
+Action onActivate = () => 
+{
+    Application.RegisterResources();
+    var builder = Builder.FromResource("/org/gtk/example/window.ui");
+    Builder.AddFromFile(builder, "builder.ui");
+    var window = Builder.GetObject(builder, "window");
+    Window.SetApplication(window, app);
+    GObject.Unref( builder);
+    Widget.Show(window);
+
+    var files = System.Environment.CommandLine.Split(' ').Skip(1).Select(GFile.New);
+    var file = files.First();
+    var name = GFile.GetBasename(file);
+};
+
+var status = Application.Run(app, onActivate);
+
+GObject.Unref(app);
+
+return status;
+
