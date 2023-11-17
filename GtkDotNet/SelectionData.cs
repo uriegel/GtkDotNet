77f62f4f8e69759fbf52b0d54b1cdca198c6e121
Uwe Riegel <uriegel@hotmail.de>
2023-11-18 00:39:26 +0100

    Selctiondata url encode

--- GtkDotNet/SelectionData.cs
+++ GtkDotNet/SelectionData.cs
@@ -1,10 +1,20 @@
 
 using System;
+using System.Linq;
 using System.Runtime.InteropServices;
 using GtkDotNet;
+using LinqTools;
 
 public static class SelectionData
 {
+    public static IntPtr DataSetUris(this IntPtr data, string[] uris)
+    {
+        var intptrs = uris.Select(n => Marshal.StringToCoTaskMemUTF8(Uri.EscapeDataString(n).Replace("%2F", "/").Replace("%3A", ":"))).ToArray();
+        var result = DataSetUris(data, intptrs);
+        intptrs.ForEach(n => Marshal.FreeCoTaskMem(n));
+        return result;
+    }
+
     [DllImport(Globals.LibGtk, EntryPoint = "gtk_selection_data_set_uris", CallingConvention = CallingConvention.Cdecl)]
-    public extern static IntPtr DataSetUris(this IntPtr data, string[] uris);
+    extern static IntPtr DataSetUris(this IntPtr data, IntPtr[] uris);
 }
